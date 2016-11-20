2c8b0ef437a5cbff2b3cb82a1e4ef4cbf3308318
clindsd8 <drew.alexander.lindsey@jci.com>
2016-11-20 00:40:10 -0600

    Pipeline and SEEF

--- src/pipeline/AbstractPipeline.py
+++ src/pipeline/AbstractPipeline.py
@@ -1,6 +1,6 @@
-class AbstractSeef(object):
-	def __init__(self)
+class AbstractPipeline(object):
+	def __init__(self, sequence):
 		raise Exception('Abstract class has no implementation')
 		
-	def computeEnergy(self, conformation):
+	def generateDeNovoPrediction():
 		raise Exception('Abstract class has no implementation')
