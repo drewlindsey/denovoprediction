fa31199ab84eaf74db3eb48ffcfae98f75b0bf06
DrewLindsey <drew.alexander.lindsey@jci.com>
2016-11-21 20:26:30 -0600

    AbstractConformationSampler documentation

--- src/conformation/AbstractConformationSampler.py
+++ src/conformation/AbstractConformationSampler.py
@@ -1,14 +1,30 @@
 class AbstractConformationSampler(object):
+	"""An abstract base class for the ConformationSampler. 
+	
+	Controls sampling the conformation space and testing the energy. Acts
+	as though it is an iterator and can be continually iterated upon until
+	the hasNext conditions have been completed.
+	
+	Attributes:
+		initialConformation: The initial backbone conformation
+		seefModel: The SEEF
+		fragLib: The FragmentLibrary object to sample from
+	"""
+	
 	def __init__(self, initialConformation, seefModel, fragLib):
+		"""Inits the ConformationSampler"""
 		self.conformation = initialConformation
 		self.seef = seefModel
 		self.fragLib = fragLib
 
 	def next(self):
+		"""Generate the next conformation"""
 		raise Exception('Abstract class has no implementation')
 		
 	def hasNext(self):
+		"""Checks if the sampling is complete"""
 		raise Exception('Abstract class has no implementation')
 		
 	def minimum(self):
+		"""The current minima conformation"""
 		raise Exception('Abstract class has no implementation')
