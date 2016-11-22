d0d1c646de6e0ffe6044158de5d5c2abc865a4f7
clindsd8 <drew.alexander.lindsey@jci.com>
2016-11-21 19:15:11 -0600

    AbstractFragmentLibraryGenerator documentation

--- src/fragments/AbstractFragmentLibraryGenerator.py
+++ src/fragments/AbstractFragmentLibraryGenerator.py
@@ -1,6 +1,15 @@
 class AbstractFragmentLibraryGenerator(object):
+	"""An abstract base class for generating the FragmentLibrary
+	
+	Attributes:
+		sequence: The sequence used to generate the nearest neighbor fragment library
+		residueMapper: The residue mapping class to generate residues from the ROBETTA library (or some other source)
+	"""
+	
 	def __init__(self, sequence, residueMapper):
+		"""Inits the fragment library generator"""
 		raise Exception('Abstract class has no implementation')
 		
 	def generateLibrary(self):
+		"""Generates the FragmentLibrary for the classes sequence"""
 		raise Exception('Abstract class has no implementation')
