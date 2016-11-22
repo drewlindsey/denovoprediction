4a278f2a8a9d0ec7c62a26b45334ea57680c4958
clindsd8 <drew.alexander.lindsey@jci.com>
2016-11-21 18:31:26 -0600

    AbstractSeef documentation

--- src/seef/AbstractSeef.py
+++ src/seef/AbstractSeef.py
@@ -1,6 +1,14 @@
 class AbstractSeef(object):
-	def __init__(self)
+	"""An abstract class template for the Statistical Effective Energy Function (SEEF)
+	
+	Attributes:
+		options: the options for the SEEF function (if any)
+	"""
+	
+	def __init__(self, options=None):
+		"""Inits the SEEF with the given (optional) options"""
 		raise Exception('Abstract class has no implementation')
 		
 	def computeEnergy(self, conformation):
+		"""Computes the energy of the given conformation as described by the implementation details"""
 		raise Exception('Abstract class has no implementation')
