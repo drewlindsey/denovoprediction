bcfdcf98df32590d49168093c147aa63f5138741
DrewLindsey <drew.alexander.lindsey@jci.com>
2016-11-21 20:35:48 -0600

    AbstractConformationInitializer documentation

--- src/conformation/AbstractConformationInitializer.py
+++ src/conformation/AbstractConformationInitializer.py
@@ -1,6 +1,15 @@
 class AbstractConformationInitializer(object):
+	"""An abstract base class for conformation initialization. Determines
+	how the conformation is initialized, e.g. straight-line, 'nearly' straight-line,
+	or a 'match' from the PDB
+	
+	Attributes:
+		sequence: The sequence to initialize into a list of Residue objects
+	"""
 	def __init__(self, sequence):
+		"""Inits the Conformation Initializer"""
 		raise Exception('Abstract class has no implementation')
 	
 	def generateConformation(self):
+		"""Generates the initialized conformation using this class' methods"""
 		raise Exception('Abstract class has no implementation')
