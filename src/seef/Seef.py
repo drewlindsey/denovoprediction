fbda2a6477c01e639750312b91c3509452e510c0
drewlindsey <drew.a.lindsey@gmail.com>
2016-11-23 02:15:06 -0600

    RW Potential SEEF added, but only works on linux with CalRW on users' end

--- src/seef/Seef.py
+++ src/seef/Seef.py
@@ -1,20 +1,41 @@
 from abc import ABCMeta, abstractmethod
+import subprocess as sp
+import re
 
 
 class BaseSeef(object):
-	__metaclass__ = ABCMeta
-	"""An abstract class template for the Statistical Effective Energy Function (SEEF)
-	
-	Attributes:
-		options: the options for the SEEF function (if any)
-	"""
+    __metaclass__ = ABCMeta
+    """An abstract class template for the Statistical Effective Energy Function (SEEF)
 
-	@abstractmethod
-	def __init__(self, options=None):
-		"""Inits the SEEF with the given (optional) options"""
-		pass
+    Attributes:
+        options: the options for the SEEF function (if any)
+    """
 
-	@abstractmethod
-	def compute_energy(self, conformation):
-		"""Computes the energy of the given conformation as described by the implementation details"""
-		pass
+    @abstractmethod
+    def __init__(self, *args):
+        """Inits the SEEF with the given (optional) options"""
+        pass
+
+    @abstractmethod
+    def compute_energy(self, conformation):
+        """Computes the energy of the given conformation as described by the implementation details"""
+        pass
+
+
+class RwPotential(BaseSeef):
+    """A SEEF function requiring linux and the calRw (http://zhanglab.ccmb.med.umich.edu/RW/)
+    executable to be installed on the PATH or in the directory calling this function.
+    """
+    def __init__(self):
+        super(RwPotential, self).__init__()
+
+    def compute_energy(self, conformation):
+        """Generates the energy value for the given conformation using RW Potential executable file
+
+        conformation:
+            A PDB file to determine energy from
+        """
+        rwPotentialCall = sp.Popen(['./calRW', conformation], stdout=sp.PIPE, stderr=sp.PIPE)
+        rwPotentialString, err = rwPotentialCall.communicate()
+        rwPotentialValue = re.search("-?([0-9]+\.[0-9]+)", rwPotentialString).group(0)
+        return float(rwPotentialValue)
