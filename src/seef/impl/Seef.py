4b8b1c7a8a4b1fd4c68fbf519682be2d1e12b70d
drewlindsey <drew.a.lindsey@gmail.com>
2016-11-23 02:16:18 -0600

    Remove unnecessary files

--- src/seef/impl/Seef.py
+++
@@ -1,9 +0,0 @@
-class Seef(AbstractSeef):
-	"""A simple SEEF implementation"""
-	def __init__(self):
-		"""Inits the SEEF object"""
-		raise Exception('Not implemented exception')
-		
-	def computeEnergy(self, conformation):
-		"""Generates the energy value for the given conformation"""
-		raise Exception('Not implemented exception')
