5cedb59b9aebd504842788c2277f4df8ec698090
drewlindsey <drew.a.lindsey@gmail.com>
2016-11-28 17:05:31 -0600

    randint is inclusive on both ends. adjust params

--- src/conformation/ConformationSampler.py
+++ src/conformation/ConformationSampler.py
@@ -70,8 +70,8 @@ class ConformationSampler(BaseConformationSampler):
         """Generates the next conformation using the metropolis algorithm"""
         dummy = self.conformation
         print "dummy length : " + str(dummy.get_length())
-        startPos = random.randint(0, dummy.get_length())
-        rand_neighbor = random.randint(0, 200)
+        startPos = random.randint(0, dummy.get_length()-1)
+        rand_neighbor = random.randint(0, 199)
 
         prob9 = (self.temp - self.minTemp) / (self.maxTemp - self.minTemp)
         count = 9 if prob9 > random.random() else 3
