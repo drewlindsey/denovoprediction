66ced0ff04a00b4497ca17a92a6c60f014395fae
drewlindsey <drew.a.lindsey@gmail.com>
2016-11-28 21:17:28 -0600

    pass file name to lipa

--- src/mapper.py
+++ src/mapper.py
@@ -75,7 +75,7 @@ def map_conformation_to_pdb(conformation, loc):
         tmp.write("{0} {1} {2} {3}\n".
                   format(residue.get_type(), angles["phi"], angles["psi"], angles["omega"]))
     file_name = os.path.join(loc, conformation.get_name() + "-" + str(uuid.uuid1()) + ".pdb")
-    lipa_call = sp.Popen(['lipa', tmp, '>', file_name], stdout=sp.PIPE, stderr=sp.PIPE)
+    lipa_call = sp.Popen(['lipa', tmp.name, '>', file_name], stdout=sp.PIPE, stderr=sp.PIPE)
     lipa_out, err = lipa_call.communicate()
 
     print lipa_out
